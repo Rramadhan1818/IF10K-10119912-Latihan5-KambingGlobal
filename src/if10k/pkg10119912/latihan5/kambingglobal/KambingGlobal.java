d46697828a0e7e18fd3ff021fef73dc13a43fb5c
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-18 15:47:12 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan5/kambingglobal/KambingGlobal.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan5.kambingglobal;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : Program ini berisi proses set dan get method
+ * 
+ */
+public class KambingGlobal {
+
+    int jumlahKambing = 88;
+    
+    // Method untuk ,enampilkan jumlah kambing
+    public void getJumlahKambing() {
+        System.out.println("Jumlah kambing: " + jumlahKambing);
+    }
+    public void tambahKambing() {
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        KambingGlobal kambingSusu = new KambingGlobal();
+        
+        //Menampilkan jumlah kambing yang ada saat program pertama x berjalan
+        kambingSusu.getJumlahKambing();
+        
+        //Menambah satu kambing
+        kambingSusu.tambahKambing();
+        
+        //Menampilkan jumlah kambing yang ada
+        kambingSusu.getJumlahKambing();
+    }
+    
+}
